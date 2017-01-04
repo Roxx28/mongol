8c7d869571637d54ecf441018b866e9e6389471a
Tudor Marghidanu <tudor@marghidanu.com>
2017-01-04 15:10:33 +0200

    Relations foundation

---
+++ lib/Mongol/Roles/Relations.pm
@@ -0,0 +1,47 @@
+package Mongol::Roles::Relations {
+	use Moose::Role;
+
+	use Class::Load qw( load_class );
+
+	sub has_many {
+		my ( $class, $type, $foreign_key, $config ) = @_;
+	}
+
+	sub has_one {
+		my ( $class, $type, $foreign_key, $config ) = @_;
+	}
+
+	no Moose::Role;
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Mongol::Roles::Relations - Automatic relations builder
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 has_many
+
+=head2 has_one
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+L<MongoDB>
+
+=back
+
+=cut
